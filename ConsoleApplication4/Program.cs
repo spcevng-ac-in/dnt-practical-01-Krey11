d83b173e5e0785fbd3ee4e94f211b05ce5aa07ed
Krey11 <2019095900007092@spu.ac.in>
2022-01-07 13:38:13 +0530

    Submit First Practical

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+            Console.WriteLine("Enter x:");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+
+            Console.Write("Sum is= " + sum);
+            Console.Read();
+        }
+    }
+}
+       
